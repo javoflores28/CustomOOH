6523decdb7249a2ecc6a73cd137996afb137142f
Javier Flores <javoflores28@hotmail.com>
2021-04-15 15:38:31 -0500

    Mobiliario Urbano y Grandes formatos

---
+++ CustomOOH/View Controllers/MobiliarioUrbano/SearchBarUrbanos.swift
@@ -0,0 +1,14 @@
+//
+//  SearchBarUrbanos.swift
+//  CustomOOH
+//
+//  Created by Javier Flores on 15/04/21.
+//  Copyright © 2021 mx.itesm.A01651678. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarUrbanos: UICollectionReusableView {
+        
+    @IBOutlet weak var SearchBarUrbanos: UISearchBar!
+}
